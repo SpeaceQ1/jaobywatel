58715c65407680f44ebe0e379405db1f2e3d958c
SpeaceQ1 <mr.dawid2008@gmail.com>
2025-03-13 22:43:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const FILES_TO_CACHE = [
+  "/jaobywatel/",  // Główna strona aplikacji
+  "/jaobywatel/index.html",  // Strona index
+  "/jaobywatel/icon-192.png",  // Ikona 192x192
+  "/jaobywatel/icon-512.png"  // Ikona 512x512
+];
+
+// Instalacja Service Workera i zapisanie plików w cache
 self.addEventListener("install", (event) => {
   event.waitUntil(
-   caches.open("app-cache").then((cache) => {
-      return cache.addAll([
-       "/jaobywatel/",
-        "/jaobywatel/index.html",
-        "/jaobywatel/style.css",
-        "/jaobywatel/script.js",
-       "/jaobywatel/icon-192.png",
-        "/jaobywatel/icon-512.png"
-      ]);
-   })
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
+// Obsługa żądań - zwracanie plików z cache
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-     return response || fetch(event.request);
+      return response || fetch(event.request);
     })
   );
 });
